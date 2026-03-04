8fe88727969ca323081e4cb5e90d1cb7c256ec2b
Willian Galvez Fernández <willian.galvez91@gmail.com>
2026-03-04 11:02:37 +0100

    feat: ng add schematic logic

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,10 +1,29 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Rule, SchematicContext, SchematicsException, TaskId, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask, RunSchematicTask } from '@angular-devkit/schematics/tasks';
+import { getPackageJsonDependency } from '@schematics/angular/utility/dependencies';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function ngAdd(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+export function ngAdd(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+
+    const dep = getPackageJsonDependency(tree, '@angular/core');
+
+    if (!dep) {
+      context.logger.error('@angular/core package missing');
+      return tree;
+    }
+
+    let taskId: TaskId | undefined = context.addTask(new RunSchematicTask('install-packages', {}));
+
+    if (!taskId) {
+      throw new SchematicsException(
+        'No se ha ejecutado ninguna tarea de aprovisionamiento'
+      );
+    }
+
+    context.addTask(new NodePackageInstallTask(), [taskId]);
+
     return tree;
   };
 }
