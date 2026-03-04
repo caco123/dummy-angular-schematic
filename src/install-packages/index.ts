1ed75d25ef3d3a40991e162670e2710363edee1c
Willian Galvez Fernández <willian.galvez91@gmail.com>
2026-03-04 11:02:10 +0100

    feat: install packages schematic

--- src/install-packages/index.ts
+++ src/install-packages/index.ts
@@ -1,10 +1,17 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Rule, Tree, chain } from '@angular-devkit/schematics';
+import { NodeDependencyType, addPackageJsonDependency } from '@schematics/angular/utility/dependencies';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function installPackages(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return chain([
+    installPackage(NodeDependencyType.Default, 'ngx-caco-image-splitter', 'latest'),
+  ])
+}
+
+export function installPackage(type: NodeDependencyType, name: string, version: string): Rule {
+  return (tree: Tree) => {
+    addPackageJsonDependency(tree, { type, name, version });
     return tree;
   };
 }
